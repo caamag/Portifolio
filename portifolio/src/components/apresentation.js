c6005f4d84ed07201de03d49897c109627502092
Caio Lopes <caiolopesfv@gmail.com>
2023-12-06 21:55:52 -0300

    enviando atualizações

---
+++ portifolio/src/components/apresentation.js
@@ -0,0 +1,18 @@
+import './apresentation.css'; 
+
+
+function Apresentation () {
+
+    return (
+
+        <div className="apresentation-container">
+
+            <div className="photo"></div>
+
+        </div>
+
+    )
+
+}
+
+export default Apresentation; 
